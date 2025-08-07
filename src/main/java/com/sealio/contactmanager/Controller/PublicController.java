338bedd1ac340e63e0aba15af0b2e69e3290c811
luminous44 <aklemasultanakhi@gmail.com>
2025-08-07 12:00:52 +0600

    added health checker

---
+++ src/main/java/com/sealio/contactmanager/Controller/PublicController.java
@@ -0,0 +1,17 @@
+package com.sealio.contactmanager.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/public")
+public class PublicController {
+
+    @GetMapping("/health")
+    public String health(){
+        return "Ok";
+    }
+
+
+}
